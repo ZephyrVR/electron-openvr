76ae72301bac23f5b69e40a3ab4cb2bff3ccb667
Thomas Gaubert <tpgaubert@gmail.com>
2018-09-02 18:59:25 -0700

    Add icon path

--- index.js
+++ index.js
@@ -24,24 +24,20 @@ class VRWindow extends BrowserWindow {
     this.__setupOverlay(vrOpts)
   }
 
-  __setupOverlay ({ key, name = 'Electron VR App', fps = 60, system = null }) {
+  __setupOverlay ({ key, name = 'Electron VR App', fps = 60, iconPath, system = null }) {
     this.__vrSystem = system || vr.system.VR_Init(vr.EVRApplicationType.VRApplication_Overlay)
-    this.overlay = new VROverlay({ system: this.__vrSystem, key: `electronvr:${Date.now()}:${key}`, name: name })
+    this.overlay = new VROverlay({ system: this.__vrSystem, key: key, name: name, iconPath: iconPath })
     this.webContents.setFrameRate(fps)
-    
+
     this.webContents.on('paint', (...args) => {
       this.__vrDraw()
     })
 
     this.webContents.on('dom-ready', () => {
-      // setup overlay further
-      // this.overlay.transformTrackedDeviceRelative(device, { x, y, z })
       this.overlay.show()
 
-      // initial force draw, this is usually empty but fuck it.
       this.__vrDraw()
 
-      // force draw after a second, too
       setTimeout(() => {
         this.__vrDraw()
       }, 1000)
@@ -55,7 +51,7 @@ class VRWindow extends BrowserWindow {
       if (buf.length === 0) {
         return
       }
-      
+
       this.overlay.setTextureFromBuffer(buf, { ...image.getSize() })
     })
   }
